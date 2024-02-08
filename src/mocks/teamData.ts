0728b9d9a68a167313bca7698d34a31d205c8111
mateussousaa <mateussousa20@outlook.com>
2024-02-08 20:32:33 -0300

    feat: randomizer-soccer v1 implementation

---
+++ src/mocks/teamData.ts
@@ -0,0 +1,15 @@
+export type TeamData = {
+  name: string;
+  league: string;
+  country: string;
+  position: number;
+  crest: string
+}; 
+
+export const teamDataMock: TeamData = {
+  name: 'Name',
+  league: 'League',
+  country: 'Country',
+  position: 1,
+  crest: 'assets/interrogation.svg'
+}
